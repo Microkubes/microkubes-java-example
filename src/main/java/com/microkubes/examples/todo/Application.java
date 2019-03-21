44cf7a985a813e94a53f779338a1d2a959372ea0
Pavle Jonoski <pavle.jonoski@keitaro.com>
2019-03-21 16:16:51 +0100

    WIP: :construction: Using Microkubes AutoRegister configuration.

--- src/main/java/com/microkubes/examples/todo/Application.java
+++ src/main/java/com/microkubes/examples/todo/Application.java
@@ -1,10 +1,16 @@
 package com.microkubes.examples.todo;
 
+import com.microkubes.tools.gateway.spring.RegistrationConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@EnableAutoConfiguration()
 @SpringBootApplication
+@Import(RegistrationConfiguration.class)
 public class Application {
+
     public static void main(String [] args){
         SpringApplication.run(Application.class, args);
     }
